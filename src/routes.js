a2d2ab6883e4b2482d4a50d0fb56e6d1999e2252
aninnnnha <ana.furtado32@aluno.ce.gov.br>
2025-05-13 11:37:34 -0300

    v1

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import candidatoController from './controllers/candidatoController.js';
+
+const router = Router();
+router.get("/", (req, res) =>{
+    res.status(200).send("Pagina HOME")
+})
+
+router.post("/alunos", candidatoController.Inserir)
+router.get("/alunos", candidatoController.Listar)
+router.put("/alunos/:id", candidatoController.Editar)
+router.delete("/alunos/:id", candidatoController.Excluir)
+export default router;
