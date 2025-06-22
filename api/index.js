7aba4a0e5dcaaa89344fd7c6c29d7247e8ea63c4
aninnnnha <ana.furtado32@aluno.ce.gov.br>
2025-06-22 16:56:32 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
+// api/index.js
 import express from 'express';
 import router from './routes.js';
 import cors from 'cors';
@@ -6,15 +7,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-
-// Configure o CORS conforme seu front-end, aqui está aberto para todos (prod ajuste!)
 app.use(cors());
-
-// Permite receber JSON no corpo das requisições
 app.use(express.json());
-
-// Use as rotas definidas
 app.use(router);
 
-// Exporta o app para o Vercel rodar como função serverless
+// Vercel exige isso: exportar o handler da API
 export default app;
