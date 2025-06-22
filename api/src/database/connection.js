c3fde28d8c51ee76edda043290c5b38f7b913b42
aninnnnha <ana.furtado32@aluno.ce.gov.br>
2025-06-22 16:45:39 -0300

    Update connection.js

--- api/src/database/connection.js
+++ api/src/database/connection.js
@@ -1,17 +1,22 @@
 import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 
-// Carrega as variáveis do .env
 dotenv.config();
 
-const connection = await mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    port: process.env.PORT,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
-});
+let connection;
 
-console.log("Conectado ao banco de dados com sucesso!");
+async function connect() {
+  if (!connection) {
+    connection = await mysql.createConnection({
+      host: process.env.MYSQL_HOST,
+      port: process.env.PORT,
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE
+    });
+    console.log("Conectado ao banco!");
+  }
+  return connection;
+}
 
-export default { connection };
+export default { getConnection: connect };
