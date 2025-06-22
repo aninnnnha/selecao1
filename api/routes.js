6d067ff9269828a7e7a70444b8ad73ae02f23c4c
aninnnnha <ana.furtado32@aluno.ce.gov.br>
2025-06-22 16:55:06 -0300

    Update routes.js

--- api/routes.js
+++ api/routes.js
@@ -1,12 +1,17 @@
 import { Router } from "express";
 import candidatoController from "./src/controllers/usuarioController.js";
-const router = Router();
-router.get("/", (req, res) =>{
-    res.status(200).send("Pagina HOME")
-})
 
-router.post("/alunos", candidatoController.Inserir)
-router.get("/alunos", candidatoController.Login)
-// router.put("/alunos/:id", candidatoController.Editar)
-// router.delete("/alunos/:id", candidatoController.Excluir)
+const router = Router();
+
+router.get("/", (req, res) => {
+  res.status(200).send("Página HOME");
+});
+
+// Rotas de alunos
+router.post("/alunos", candidatoController.Inserir);
+router.get("/alunos", candidatoController.Listar); // listar todos os alunos (crie essa função no controller)
+
+// Rota de login
+router.post("/login", candidatoController.Login);
+
 export default router;
